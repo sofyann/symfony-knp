58309e5203b0adcc0e9a2a3646816c3333f09204
Unknown <sofyann.fahim@gmail.com>
2017-11-16 18:29:17 +0100

    doctrine_cache

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -23,7 +23,16 @@ class GenusController extends Controller
     public function showAction($genusName){
 
         $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
-        $funFact = $this->container->get('markdown.parser')->transform($funFact);
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+        if ($cache->contains($key)){
+            $funFact = $cache->fetch($key);
+        } else {
+            sleep(1);
+            $funFact = $this->container->get('markdown.parser')->transform($funFact);
+            $cache->save($key, $funFact);
+        }
+
         return $this->render('genus/show.html.twig', [
                 'name' => $genusName,
                 'funFact' => $funFact
