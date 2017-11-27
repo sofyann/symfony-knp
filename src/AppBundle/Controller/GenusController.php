98ff21456eab742d6a769604ac26af339cb53058
Unknown <sofyann.fahim@gmail.com>
2017-11-27 10:55:53 +0100

    Utilisation de Doctrine pour les BDD
    Ajout de la page list qui affiche la liste de tous les genus présents dans la BDD, show est maintenant dynamique.

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -18,24 +19,53 @@ use Symfony\Component\HttpFoundation\Response;
 class GenusController extends Controller
 {
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/new")
+     */
+    public function newAction(){
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1,100));
+        $genus->setSubFamily('Octopodinae');
+        $genus->setSpeciesCount(rand(100, 99999));
+        $em= $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('Genus Created!');
+
+    }
+
+    /**
+     * @Route("/genus")
+     */
+    public function listAction(){
+        $em = $this->getDoctrine()->getManager();
+        $genuses = $em->getRepository('AppBundle:Genus')->findAll();
+        dump($genuses);
+        return $this->render('genus/list.html.twig', ['genuses' => $genuses]);
+    }
+    /**
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName){
 
-        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
-        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
-        $key = md5($funFact);
-        if ($cache->contains($key)){
-            $funFact = $cache->fetch($key);
-        } else {
-            sleep(1);
-            $funFact = $this->container->get('markdown.parser')->transform($funFact);
-            $cache->save($key, $funFact);
+        $em= $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')->findOneBy(['name'=>$genusName]);
+        if(!$genus){
+            throw  $this->createNotFoundException('Not genus found');
         }
 
+//        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+//        $key = md5($funFact);
+//        if ($cache->contains($key)){
+//            $funFact = $cache->fetch($key);
+//        } else {
+//            sleep(1);
+//            $funFact = $this->container->get('markdown.parser')->transform($funFact);
+//            $cache->save($key, $funFact);
+//        }
+
         return $this->render('genus/show.html.twig', [
-                'name' => $genusName,
-                'funFact' => $funFact
+                'genus' =>$genus
             ]);
     }
 
